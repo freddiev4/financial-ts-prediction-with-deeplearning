70a07217512eeac805cc564596be73e2e0cb3160
Freddie Vargus <fvargus4@bu.edu>
2017-11-23 05:52:10 -0500

    MAINT: Add more details to comments and ReLU units

--- neural-network.py
+++ neural-network.py
@@ -11,13 +11,15 @@ class FinancialTimeSeriesNetwork(nn.Module):
 
     def __init__(self):
         super(FinancialTimeSeriesNetwork, self).__init__()
+        # Section 3.2; Figure 2 has an image of the architecture
         # TODO: define dimensions of each layer
+        # Section 5.2; implementation details of each layer
         # TODO: figure out what type of model to use
-        self.fc1 = nn.Linear()
-        self.fc2 = nn.Linear()
-        self.fc3 = nn.Linear()
-        self.fc4 = nn.Linear()
-        self.fc5 = nn.Linear()
+        self.fc1 = nn.Linear(500)
+        self.fc2 = nn.Linear(200)
+        self.fc3 = nn.Linear(40)
+        self.fc4 = nn.Linear(20)
+        self.fc5 = nn.Linear(2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -35,18 +37,26 @@ class FinancialTimeSeriesNetwork(nn.Module):
 net = FinancialTimeSeriesNetwork()
 print(net)
 
+# Section 3.3 - Preprocessing
+# Section 3.4 - Labeling the Dataset
 # TODO: define the input here
 # using minutely prices
 # along with the labels for the data
+
 
 # Thinking that maybe I should use Zipline here for actually
 # running a backtest & loading the data
 # in which case I'd either have to use the Quandl bundle
 # or create a custom bundle that loads IEX Trading data
+
+# Section 5; Experimental Results
+# Test Set: June 23rd 2014 to June 22nd 2016
+# Validation Set: June 23rd 2013 to June 22nd 2014
+# Training Set: all previous data
 trainset = None
 trainloader = torch.utils.data.DataLoader(
     trainset,
-    batch_size=4,
+    batch_size=100,  # Section 5.2; batch size of 100
     shuffle=False,
     num_workers=2
 )
@@ -54,14 +64,20 @@ trainloader = torch.utils.data.DataLoader(
 testset = None
 testloader = torch.utils.data.DataLoader(
     testset,
-    batch_size=4,
+    batch_size=100,  # Section 5.2; batch size of 100
     shuffle=False,
     num_workers=2
 )
 
+# "Our model does not account for changes in the market dynamics, and
+# implicitly assumes that a strategy learnt using the data up to 2013,
+# can be applied to trades in 2016."
 
-# use Stochastic Gradient Descent as the optimizer
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+# Section 5.2; initial learning rate of 0.001
+# "We apply Stochastic Gradient Descent to train the models
+# with 100 batch size and 0.5 dropout rate"
+optimizer = optim.SGD(net.parameters(), lr=0.001)
 criterion = nn.Softmax()
 
 
@@ -74,8 +90,7 @@ criterion = nn.Softmax()
 running_loss = []
 for epoch in range(1):
     for i, data in enumerate(trainloader, 0):
-        # XXX: I don't think we're actually using labels here
-        # as it is time-series data
+        # XXX: Section 3.4 & Section 5.1 talk about labeling the data
         inputs, labels = data
         inputs, labels = Variable(inputs), Variable(labels)
 
